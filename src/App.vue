80d29d9e756213591bae7afdd865b6ef3dc1ffce
AlexandrNakaryakov <sasha080603@gmail.com>
2023-06-25 00:14:14 +0500

    commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld :name="prod.Name" :elements="prod.Elements" :depht="0"/>
+</template>
+
+<script>
+import products from "./products.json"
+import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+  data(){
+    return {
+      prod: products
+    }
+  },
+  components:{
+    HelloWorld
+  },
+  methods:{
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div >
+    <div :style="indient" @click="categoryClick" class="store">{{ name }}</div>
+  </div>
+  <div>
+    <div v-if="opened">
+      <HelloWorld
+      v-for="el in elements" 
+      :elements="el.Elements" 
+      :name="el.Name"
+      :depht="depht+1"
+      />
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return{
+        opened: false
+      }
+    },
+    props: [ 'name', 'elements', 'depht' ],
+    name: 'HelloWorld',
+    methods:{
+      categoryClick(){
+        this.opened = !this.opened
+      }
+    },
+    computed: {
+      indient() {
+        return this.elements ? { 
+            transform: `translate(${this.depht * 50}px)`,
+            cursor: 'pointer'
+          } : {
+            transform: `translate(${this.depht * 50}px)`,
+            cursor: 'default'
+          }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
